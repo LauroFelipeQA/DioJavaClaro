ec85ab83f11b2cbf2c90ecb9d4c0ad61425ab290
lauro.neto <lauro.neto.ext@sascar.com.br>
2024-07-31 15:23:59 -0300

    First Commit

---
+++ src/ContaTerminal.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+
+        System.out.println("Por favor, digite o número da agência !");
+        numero = scanner.nextInt();
+        scanner.nextLine(); //
+
+        System.out.println("Por favor, digite o número da conta !");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor, digite o seu nome!");
+        nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite valor do deposito");
+        saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, " +
+                "sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível" +
+                " para saque.");
+
+        scanner.close();
+    }
+}
